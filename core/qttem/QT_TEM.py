de531392972fb078eb43fa79edcb50e6a5fecd6c
lijunhua <634134551@qq.com>
2023-09-13 23:42:17 +0800

    9/13

--- core/qttem/QT_TEM.py
+++ core/qttem/QT_TEM.py
@@ -1,7 +1,11 @@
 import sys
-from PyQt6.QtWidgets import (QWidget, QToolTip,
-    QPushButton, QApplication,QMainWindow,QMenu)
-from PyQt6.QtGui import QIcon, QAction
+from PyQt6.QtCore import *
+from PyQt6.QtWidgets import *
+from PyQt6.QtGui import *
+
+#from PyQt6.QtCore import Qt
+# from PyQt6.QtWidgets import (QWidget, QToolTip,QPushButton, QApplication,QMainWindow,QMenu,QFrame,QVBoxLayout, QSplitter)
+# from PyQt6.QtGui import QIcon, QAction
 
 
 class QT_INIT(QMainWindow):
@@ -10,33 +14,37 @@ class QT_INIT(QMainWindow):
         super().__init__()
 
         self.initUI()
+        self.createLeftMenu()
 
     def initUI(self):
+        self.setWindowTitle('Peristaltic pump flow control interface')
+        self.setGeometry(300, 300, 1000, 800)
 
-        menubar = self.menuBar()
-        fileMenu1 = menubar.addMenu('File')     #主菜单
-        fileMenu2 = menubar.addMenu('Exit')
+        # 创建左侧菜单栏
+        left_menu = self.createLeftMenu()
 
+        # 创建内容窗口
+        content_frame = QFrame(self)
+        content_frame.setStyleSheet("background-color: #2E4053;")
 
-        impMenu = QMenu('Import', self)         #子菜单
-        impAct = QAction('Import mail', self)   #从菜单动作
-        impMenu.addAction(impAct)               #从子菜单进入到从菜单
+        # 创建一个分割窗口，左侧是菜单栏，右侧是内容窗口
+        splitter = QSplitter(Qt.Orientation.Horizontal)
+        splitter.addWidget(left_menu)
+        splitter.addWidget(content_frame)
+        # 设置分割条的位置
+        splitter.setSizes([150, 200])
 
-        newAct = QAction('New', self)
+        self.setCentralWidget(splitter)
 
-        fileMenu1.addAction(newAct)
-        fileMenu1.addMenu(impMenu)
-
-        self.setGeometry(300, 300, 350, 250)
-        self.setWindowTitle('Submenu')
         self.show()
 
-    def center(self):
 
-        qr = self.frameGeometry()
-        cp = self.screen().availableGeometry().center()
 
-        qr.moveCenter(cp)
-        self.move(qr.topLeft())
+
+
+
+
+
+
 
 

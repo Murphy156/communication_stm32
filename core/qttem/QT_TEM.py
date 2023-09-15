da5ab0489c361918902e92edebbb6ee198c09355
lijunhua <634134551@qq.com>
2023-09-15 13:34:58 +0800

    9/15

--- core/qttem/QT_TEM.py
+++ core/qttem/QT_TEM.py
@@ -11,9 +11,78 @@ class SubWindow1(QDialog):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("子窗口")
-        self.setGeometry(300, 300, 1000, 800)
+        self.setGeometry(300, 200, 10, 10)
+        # 设置子窗口的背景颜色
+        self.setStyleSheet("background-color: lightblue;")
+        # 创建水平布局
+        layout = QHBoxLayout()
 
+        # 创建实线框架（QFrame）作为分隔区域
+        separator1 = QFrame()
+        separator1.setFrameShape(QFrame.Shape.Box)
+        separator1.setFrameShadow(QFrame.Shadow.Sunken)
+
+        # 向布局添加分隔区域和一些文本或其他控件
+        layout.addWidget(separator1)
+        layout.addWidget(QWidget())  # 用于占位的小部件
+        layout.addWidget(QWidget())  # 用于占位的小部件
+
+        # 将布局设置为子窗口的布局
+        self.setLayout(layout)
+
+class SubWindow2(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        self.setGeometry(300, 200, 10, 10)
+        # 设置子窗口的背景颜色
+        self.setStyleSheet("background-color: lightgreen;")
+# 创建垂直布局
+        layout = QVBoxLayout()
+
+        # 创建实线框架（QFrame）作为分隔区域
+        separator = QFrame()
+        separator.setFrameShape(QFrame.Shape.Box)
+        separator.setFrameShadow(QFrame.Shadow.Sunken)
+
+        # 向布局添加分隔区域和一些文本或其他控件
+        layout.addWidget(separator)
+        layout.addWidget(QWidget())  # 用于占位的小部件
+        layout.addWidget(QWidget())  # 用于占位的小部件
+
+        # 将布局设置为子窗口的布局
+        self.setLayout(layout)
+
+class SubWindow3(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        self.setGeometry(300, 200, 10, 10)
+        # 设置子窗口的背景颜色
+        self.setStyleSheet("background-color: lightblue;")
+# 创建垂直布局
+        layout = QVBoxLayout()
+
+        # 创建实线框架（QFrame）作为分隔区域
+        separator = QFrame()
+        separator.setFrameShape(QFrame.Shape.Box)
+        separator.setFrameShadow(QFrame.Shadow.Sunken)
+
+        # 向布局添加分隔区域和一些文本或其他控件
+        layout.addWidget(separator)
+        layout.addWidget(QWidget())  # 用于占位的小部件
+        layout.addWidget(QWidget())  # 用于占位的小部件
+
+        # 将布局设置为子窗口的布局
+        self.setLayout(layout)
+
+class SubWindow4(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        self.setGeometry(300, 200, 10, 10)
+        # 设置子窗口的背景颜色
+        self.setStyleSheet("background-color: lightgreen;")
 # 创建垂直布局
         layout = QVBoxLayout()
 
@@ -37,7 +106,7 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         self.setWindowTitle("Peristaltic Pump Debugging Assistant")
-        self.setGeometry(300, 300, 1000, 800)
+        self.setGeometry(300, 200, 1000, 800)
         # 创建一个主界面容器
         main_widget = QWidget()
         self.setCentralWidget(main_widget)
@@ -52,45 +121,41 @@ class MainWindow(QMainWindow):
         button1 = QPushButton("General Control Interface")
         button1.setFixedSize(300, 50)  # 设置按钮1的尺寸
         button1.clicked.connect(self.open_window1)
+        button1.setStyleSheet("background-color: gray; color: white;")
+        button_layout.addWidget(button1)
+
 
         button2 = QPushButton("Flow Observation")
         button2.setFixedSize(300, 50)  # 设置按钮2的尺寸
+        button2.clicked.connect(self.open_window2)
+        button2.setStyleSheet("background-color: gray; color: white;")
+        button_layout.addWidget(button2)
 
         button3 = QPushButton("Liquid Level Observation")
         button3.setFixedSize(300, 50)  # 设置按钮3的尺寸
+        button3.clicked.connect(self.open_window3)
+        button3.setStyleSheet("background-color: gray; color: white;")
+        button_layout.addWidget(button3)
 
         button4 = QPushButton("PID Speed Debugging")
         button4.setFixedSize(300, 50)  # 设置按钮4的尺寸
-
-        # 使用样式表设置按钮的背景颜色和字体颜色
-        button1.setStyleSheet("background-color: gray; color: white;")
-        button2.setStyleSheet("background-color: gray; color: white;")
-        button3.setStyleSheet("background-color: gray; color: white;")
+        button4.clicked.connect(self.open_window4)
         button4.setStyleSheet("background-color: gray; color: white;")
-
-        # 将按钮添加到按钮布局中
-        button_layout.addWidget(button1)
-        button_layout.addWidget(button2)
-        button_layout.addWidget(button3)
         button_layout.addWidget(button4)
+
 
         # 添加水平布局到垂直布局
         main_layout.addLayout(button_layout)
 
-        # 创建一个伸展因子，将按钮推到窗口的顶部
-        spacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
-        main_layout.addItem(spacer)
+        # # 创建一个伸展因子，将按钮推到窗口的顶部
+        # spacer = QSpacerItem(1, 1, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+        # main_layout.addItem(spacer)
 
         # 设置主窗口容器的布局为垂直布局
         main_widget.setLayout(main_layout)
 
         # 设置主窗口的背景颜色
         self.setStyleSheet("background-color: lightblack;")
-
-        # 连接按钮的点击事件到相应的槽函数
-        button2.clicked.connect(self.open_window2)
-        button3.clicked.connect(self.open_window3)
-        button4.clicked.connect(self.open_window4)
 
         # 创建一个堆叠窗口，用于显示子窗口
         self.stacked_widget = QStackedWidget()
@@ -100,18 +165,26 @@ class MainWindow(QMainWindow):
         sub_window1 = SubWindow1()
         self.stacked_widget.addWidget(sub_window1)
         self.stacked_widget.setCurrentWidget(sub_window1)
+        self.setWindowTitle("General Control Interface")
 
     def open_window2(self):
-        # 打开窗口2的代码
-        pass
+        sub_window2 = SubWindow2()
+        self.stacked_widget.addWidget(sub_window2)
+        self.stacked_widget.setCurrentWidget(sub_window2)
+        self.setWindowTitle("Flow Observation")
+
 
     def open_window3(self):
-        # 打开窗口3的代码
-        pass
+        sub_window3 = SubWindow3()
+        self.stacked_widget.addWidget(sub_window3)
+        self.stacked_widget.setCurrentWidget(sub_window3)
+        self.setWindowTitle("Liquid Level Observation")
 
     def open_window4(self):
-        # 打开窗口4的代码
-        pass
+        sub_window4 = SubWindow4()
+        self.stacked_widget.addWidget(sub_window4)
+        self.stacked_widget.setCurrentWidget(sub_window4)
+        self.setWindowTitle("PID Speed Debugging")
 
 
 
--- core/test.py
+++ core/test.py
@@ -1,28 +1,5 @@
 import sys
-from PyQt6.QtWidgets import *
-
-class SubWindow(QDialog):
-    def __init__(self):
-        super().__init__()
-
-        self.setWindowTitle("子窗口")
-        self.setGeometry(200, 200, 400, 200)
-
-        # 创建垂直布局
-        layout = QVBoxLayout()
-
-        # 创建实线框架（QFrame）作为分隔区域
-        separator = QFrame()
-        separator.setFrameShape(QFrame.Shape.Box)
-        separator.setFrameShadow(QFrame.Shadow.Sunken)
-
-        # 向布局添加分隔区域和一些文本或其他控件
-        layout.addWidget(separator)
-        layout.addWidget(QWidget())  # 用于占位的小部件
-        layout.addWidget(QWidget())  # 用于占位的小部件
-
-        # 将布局设置为子窗口的布局
-        self.setLayout(layout)
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QSpacerItem, QSizePolicy
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -38,37 +15,26 @@ class MainWindow(QMainWindow):
         # 创建垂直布局
         main_layout = QVBoxLayout()
 
-        # 创建按钮布局
-        button_layout = QVBoxLayout()
+        # 创建按钮1
+        button1 = QPushButton("按钮1")
 
-        # 创建四个按钮
-        button1 = QPushButton("窗口1")
-        button1.clicked.connect(self.show_sub_window)
+        # 创建一个垂直的伸展因子，将按钮1推到顶部
+        spacer1 = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
 
-        button2 = QPushButton("窗口2")
-        button3 = QPushButton("窗口3")
-        button4 = QPushButton("窗口4")
+        # 创建按钮2
+        button2 = QPushButton("按钮2")
 
-        # 将按钮添加到按钮布局中
-        button_layout.addWidget(button1)
-        button_layout.addWidget(button2)
-        button_layout.addWidget(button3)
-        button_layout.addWidget(button4)
+        # 创建水平的伸展因子，将按钮2推到底部
+        spacer2 = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
 
-        # 添加按钮布局到主布局
-        main_layout.addLayout(button_layout)
+        # 将控件和伸展因子添加到布局中
+        main_layout.addItem(spacer1)
+        main_layout.addWidget(button1)
+        main_layout.addItem(spacer2)
+        main_layout.addWidget(button2)
 
         # 设置主窗口容器的布局
         main_widget.setLayout(main_layout)
-
-        # 创建一个堆叠窗口，用于显示子窗口
-        self.stacked_widget = QStackedWidget()
-        main_layout.addWidget(self.stacked_widget)
-
-    def show_sub_window(self):
-        sub_window = SubWindow()
-        self.stacked_widget.addWidget(sub_window)
-        self.stacked_widget.setCurrentWidget(sub_window)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
